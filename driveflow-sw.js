6e418b1678728ab3ceda39296e2fa9b78fdd7b5d
cedric1234321-arch <cedric1234321@gmail.com>
2026-08-13 04:04:04 +0200

    Add files via upload

---
+++ driveflow-sw.js
@@ -0,0 +1,26 @@
+const CACHE="driveflow-v4-20260813";
+const ASSETS=["./","index.html","driveflow-styles.css","driveflow-app.js","driveflow.webmanifest","driveflow-icon-180.png","driveflow-icon-192.png","driveflow-icon-512.png","driveflow-brand.jpg","driveflow-deliveroo-template.csv","README_DRIVEFLOW_V4.txt"];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch",event=>{
+  const req=event.request;
+  if(req.mode==="navigate"){
+    event.respondWith(fetch(req).then(res=>{
+      const clone=res.clone();caches.open(CACHE).then(c=>c.put(req,clone));return res;
+    }).catch(()=>caches.match("./")));
+    return;
+  }
+  event.respondWith(caches.match(req).then(cached=>{
+    const network=fetch(req).then(res=>{
+      if(res&&res.ok){const clone=res.clone();caches.open(CACHE).then(c=>c.put(req,clone))}
+      return res;
+    }).catch(()=>cached);
+    return cached||network;
+  }));
+});
